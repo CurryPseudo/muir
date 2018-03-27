76bc073e30ba21a6570d36f2452d27dd3d86c486
CurryPseudo <jesonxiake@126.com>
2018-03-27 22:56:33 +0800

    Reconstruct GameObjectPuter.

---
+++ Assets/Editor/PutableGoEditor.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+using SceneController;
+using UnityEngine;
+[CustomEditor(typeof(PutableGo))]
+public class PutableGoEditor : Editor {
+    public void OnSceneGUI() {
+        PutableGo pg = (PutableGo)target;
+
+		Handles.color = Color.yellow;
+		pg.NegativePointWorld = Handles.PositionHandle(pg.NegativePointWorld, Quaternion.identity);
+		Handles.ArrowHandleCap(0, pg.NegativePointWorld, Quaternion.identity, 1f, EventType.Repaint);
+		Handles.color = Color.red;
+		pg.PositivePointWorld = Handles.PositionHandle(pg.PositivePointWorld, Quaternion.identity);
+		Handles.ArrowHandleCap(1, pg.PositivePointWorld, Quaternion.identity, 1f, EventType.Repaint);
+    }
+}
---
+++ Assets/Scripts/SceneController/PutableGo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Sirenix.OdinInspector;
+namespace SceneController {
+	public class PutableGo: MonoBehaviour {
+		public Vector2 negativePoint;
+		public Vector2 NegativePointWorld{
+			get{
+				return transform.TransformPoint(negativePoint);
+			}
+			set{
+				negativePoint = transform.InverseTransformPoint(value);
+			}
+		}
+		public Vector2 positivePoint;
+		public Vector2 PositivePointWorld{
+			get{
+				return transform.TransformPoint(positivePoint);
+			}
+			set{
+				positivePoint = transform.InverseTransformPoint(value);
+			}
+		}
+		public void PutNegativeBy(PutableGo pg) {
+			PutBy(pg.negativePoint, positivePoint);
+		}	
+		
+		public void PutPositiveBy(PutableGo pg) {
+			PutBy(pg.positivePoint, negativePoint);
+		}	
+		private void PutBy(Vector2 p1, Vector2 p2) {
+			Vector3 p1v3 = new Vector3(p1.x, p1.y, 0);
+			Vector3 position = p1v3 - transform.TransformVector(p2);
+			transform.position = position;
+		}
+		[Button("Test1")]
+		public void Test1() {
+			var pg = Instantiate(this).GetComponent<PutableGo>();
+			pg.PutNegativeBy(this);
+		}
+		[Button("Test2")]
+		public void Test2() {
+			var pg = Instantiate(this).GetComponent<PutableGo>();
+			pg.PutPositiveBy(this);
+		}
+	
+	}
+}
